ff081122cdfdee12c4f1f3029002f91c0294a75b
satuhima <78214320+satuhima@users.noreply.github.com>
2021-02-05 11:06:01 +0200

    Create kotitehtava10.php

---
+++ kotitehtava10.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+        <meta charset="utf-8">
+        <title>Calculator</title> 
+    </head>
+<?php include 'header.php'; ?>
+<?php include 'footer.php'; ?>
+
+
+<body>
+
+</html>
